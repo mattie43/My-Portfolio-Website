4944f2475ee1cbf97bd5d59e00376134bd11787e
Mattie43 <erickenm93@ymail.com>
2020-11-18 20:18:29 -0500

    need to check safari

--- src/App/components/Footer.js
+++ src/App/components/Footer.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import {
   FaGithubSquare,
   FaLinkedin,
   FaTwitterSquare,
   FaMedium,
+  FaEnvelopeSquare,
 } from "react-icons/fa";
 
 function Footer(props) {
+  const [emailCopied, setEmailCopied] = useState(false);
   const iconSize = 50;
+
+  function copyEmail() {
+    // navigator.clipboard.writeText("ericksenm93@ymail.com");
+
+    const textField = document.createElement("textarea");
+    textField.innerText = "ericksenm93@ymail.com";
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand("copy");
+    textField.remove();
+
+    setEmailCopied(true);
+    setTimeout(() => {
+      setEmailCopied(false);
+    }, 2000);
+  }
+
   return (
     <Container style={{ width: props.mobile ? "100vw" : "60vw" }}>
       <a href="https://github.com/mattie43" target="_blank" rel="noreferrer">
@@ -35,6 +54,12 @@ function Footer(props) {
       >
         <FaMedium size={iconSize} />
       </a>
+      <EmailContainer>
+        <ToolTip style={{ opacity: emailCopied ? 1 : 0 }}>
+          Email Copied!
+        </ToolTip>
+        <FaEnvelopeSquare size={iconSize} onClick={copyEmail} />
+      </EmailContainer>
     </Container>
   );
 }
@@ -48,6 +73,26 @@ const Container = styled.div`
   margin-top: 10px;
   margin-bottom: 10px;
   & a {
-    color: white;
+    color: inherit;
   }
+`;
+
+const EmailContainer = styled.div`
+  position: relative;
+  & :nth-child(2):hover {
+    cursor: pointer;
+  }
+`;
+
+const ToolTip = styled.span`
+  position: absolute;
+  z-index: 1;
+  width: 95px;
+  top: -38px;
+  left: -28px;
+  background-color: #555;
+  color: #fff;
+  border-radius: 6px;
+  padding: 4px;
+  transition: opacity 0.3s;
 `;
