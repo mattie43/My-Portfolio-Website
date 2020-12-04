0a50c63063ad7cc247730140695828036ba4e38e
Mattie43 <mattericksen93@gmail.com>
2020-12-03 21:16:12 -0500

    working transition, nav and links are not fixed

--- src/App/routes/Home.js
+++ src/App/routes/Home.js
@@ -1,16 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 import IntroImage from "../components/IntroImage";
 import Navigation from "../components/Navigation";
 import Projects from "../components/Projects";
 import ResumeContact from "../components/ResumeContact";
 import Links from "../components/Links";
-import prism from "../assests/images/prism.png";
 
 export default function Home() {
+  const pageVariant = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: "-100%",
+    },
+  };
+
+  const spring = {
+    type: "spring",
+    damping: 15,
+    stiffness: 150,
+    duration: 0.6,
+  };
+
   return (
-    <Container prism={`url(${prism})`}>
+    <Container
+      initial="out"
+      animate="in"
+      exit="out"
+      transition={spring}
+      variants={pageVariant}
+    >
       <Navigation />
       <Links />
       <IntroImage />
@@ -20,17 +44,13 @@ export default function Home() {
   );
 }
 
-const Container = styled.div`
+const Container = styled(motion.div)`
   color: #f5f5f5;
   background-color: #353535;
   min-height: 100vh;
   width: 100%;
   overflow-x: hidden;
-  display: flex;
-  flex-direction: column;
   font-size: 1.4rem;
-  background-image: ${(p) => p.prism};
-  background-size: 100vh;
   @media (min-width: 1200px) {
     font-size: 1.6rem;
   }
