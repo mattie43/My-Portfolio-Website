c22d3079a7047a099ea56e513c4dd2d8660614fc
Mattie43 <mattericksen93@gmail.com>
2020-12-03 22:03:20 -0500

    links and nav bar fixed, but memory leak issue

--- src/App/routes/Home.js
+++ src/App/routes/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
@@ -7,12 +7,24 @@ import Navigation from "../components/Navigation";
 import Projects from "../components/Projects";
 import ResumeContact from "../components/ResumeContact";
 import Links from "../components/Links";
+import Prism from "../assests/images/prism.png";
 
 export default function Home() {
+  const [transformNone, setTransformNone] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTransformNone("transform-none");
+    }, 700);
+  }, []);
+
   const pageVariant = {
     in: {
       opacity: 1,
       x: 0,
+      transitionEnd: {
+        transform: "none",
+      },
     },
     out: {
       opacity: 0,
@@ -29,11 +41,13 @@ export default function Home() {
 
   return (
     <Container
+      img={Prism}
       initial="out"
       animate="in"
       exit="out"
       transition={spring}
       variants={pageVariant}
+      className={transformNone}
     >
       <Navigation />
       <Links />
@@ -51,6 +65,8 @@ const Container = styled(motion.div)`
   width: 100%;
   overflow-x: hidden;
   font-size: 1.4rem;
+  background-image: ${(p) => `url('${p.img}')`};
+  background-size: 100vh;
   @media (min-width: 1200px) {
     font-size: 1.6rem;
   }
